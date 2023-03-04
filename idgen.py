f2e43ca072d0d863299f536deb44cae3cce78103
lvlrk <garan3160@proton.me>
2023-03-04 18:07:41 +0000

    version 1.0; shitty edition

--- idgen.py
+++ idgen.py
@@ -1,11 +1,10 @@
-#!/nix/store/h4h5rxs0hzpzvz37yrwv1k2na1acgzww-python3-3.9.15/bin/python3
+#!/usr/bin/python3
 
 __name__ = "idgen"
 __author__ = "lvlrk"
-__version__ = 0.3
+__version__ = 1.0
 
-"
-"import sys
+import sys
 import urllib3
 import codes
 import os
@@ -20,94 +19,95 @@ generatecount = 0
 lookup = False
 lookuptitle = ""
 
-usage = f"""usage: {__name__} [-g]
+usage = f"""idgen: A Nintendo GameID generator
+usage: {__name__} [-gl]
      --help               display this help
      --version            print version information
  -g, --generate [COUNT]   generate [COUNT] game IDs
  -l, --lookup [TITLE]     lookup [TITLE]"""
 
 if len(sys.argv) < 2:
-	print(usage)
-	exit(1)
+    print(usage)
+    exit(1)
 
 for i in range(len(sys.argv)):
-	if sys.argv[i] == "-g" or sys.argv[i] == "--generate":
-		if len(sys.argv) >= i + 2:
-			generate = True
-			generatecount = int(sys.argv[i + 1])
-		else:
-			print(f"{sys.argv[i]}: requires an argument [COUNT]")
-			print(usage)
-			exit(1)
-	elif sys.argv[i] == "-l" or sys.argv[i] == "--lookup":
-		if len(sys.argv) >= i + 2:
-			lookup = True
-			lookuptitle = sys.argv[i + 1]
-		else:
-			print(f"{sys.argv[i]}: requires an argument [TITLE]")
-			print(usage)
-			exit(1)
-	elif sys.argv[i] == "--help":
-		print(usage)
-		exit(0)
-	elif sys.argv[i] == "--version":
-		print(f"{__name__}-{__version__} {__author__}")
-		exit(0)
+    if sys.argv[i] == "-g" or sys.argv[i] == "--generate":
+        if len(sys.argv) >= i + 2:
+            generate = True
+            generatecount = int(sys.argv[i + 1])
+        else:
+            print(f"{sys.argv[i]}: requires an argument [COUNT]")
+            print(usage)
+            exit(1)
+    elif sys.argv[i] == "-l" or sys.argv[i] == "--lookup":
+        if len(sys.argv) >= i + 2:
+            lookup = True
+            lookuptitle = sys.argv[i + 1]
+        else:
+            print(f"{sys.argv[i]}: requires an argument [TITLE]")
+            print(usage)
+            exit(1)
+    elif sys.argv[i] == "--help":
+        print(usage)
+        exit(0)
+    elif sys.argv[i] == "--version":
+        print(f"{__name__}-{__version__} {__author__}")
+        exit(0)
 
 if os.path.exists("wiitdb.txt") == False:
-	print("warning: wiitdb.txt not found")
-	print("download from gametdb.com? [y/n]")
-	choice = input()
-	if choice == "y":
-		print("downloading database")
-		resp = http.request("GET", "https://www.gametdb.com/wiitdb.txt")
-		if resp.status != 200:
-			print(f"error: bad http request {resp.status}")
-			print(usage)
-			exit(1)
-		with open("wiitdb.txt", "w") as f:
-			f.write(resp.data.decode("utf-8"))
-			f.close()
+    print("warning: wiitdb.txt not found")
+    print("download from gametdb.com? [y/n]")
+    choice = input()
+    if choice == "y":
+        print("downloading database")
+        resp = http.request("GET", "https://www.gametdb.com/wiitdb.txt")
+        if resp.status != 200:
+            print(f"error: bad http request {resp.status}")
+            print(usage)
+            exit(1)
+        with open("wiitdb.txt", "w") as f:
+            f.write(resp.data.decode("utf-8"))
+            f.close()
 
-		print("finished without error")
-	else:
-		print("error: cannot download database")
-		exit(0)
+        print("finished without error")
+    else:
+        print("error: cannot download database")
+        exit(0)
 
 titles = []
 
 rawdb = ""
-rawdbhash = "f900c3fe8ac8d1ce5ec04ad8069bf5ec9ba35aba068b773844c5120a56b2dc56"
+#rawdbhash = "f900c3fe8ac8d1ce5ec04ad8069bf5ec9ba35aba068b773844c5120a56b2dc56"
 
 with open("wiitdb.txt", "r") as f:
-	rawdb = f.read()
-	f.close()
+    rawdb = f.read()
+    f.close()
 
 # print(hashlib.sha256(rawdb.encode("utf-8")).hexdigest())
-if hashlib.sha256(rawdb.encode("utf-8")).hexdigest() != rawdbhash:
-	print("warning: outdated database")
-	print("re-download database from gametdb.com? [y/n]")
-	choice = input()
-	if choice == "y":
-		print("re-downloading database")
-		resp = http.request("GET", "https://www.gametdb.com/wiitdb.txt")
-		if resp.status != 200:
-			print(f"error: bad http request {resp.status}")
-			print(usage)
-			exit(1)
-		with open("wiitdb.txt", "w") as f:
-			f.write(resp.data.decode("utf-8"))
-			f.close()
-
-		print("finished without error")
-	else:
-		print("error: cannot re-download database")
-		exit(0)
+#if hashlib.sha256(rawdb.encode("utf-8")).hexdigest() != rawdbhash:
+#   print("warning: outdated database")
+#   print("re-download database from gametdb.com? [y/n]")
+#   choice = input()
+#   if choice == "y":
+#       print("re-downloading database")
+#       resp = http.request("GET", "https://www.gametdb.com/wiitdb.txt")
+#       if resp.status != 200:
+#           print(f"error: bad http request {resp.status}")
+#           print(usage)
+#           exit(1)
+#       with open("wiitdb.txt", "w") as f:
+#           f.write(resp.data.decode("utf-8"))
+#           f.close()
+#
+#       print("finished without error")
+#   else:
+#       print("error: cannot re-download database")
+#       exit(0)
 
 for line in rawdb.split("\n"):
-	entry = line.split(" = ")
-	if entry[0] != "TITLES" and len(entry) >= 2:
-		titles.append({"id": entry[0], "name": entry[1]})
+    entry = line.split(" = ")
+    if entry[0] != "TITLES" and len(entry) >= 2:
+        titles.append({"id": entry[0], "name": entry[1]})
 
 #print("title_codes = [", end="")    
 #for title in titles:
@@ -115,54 +115,88 @@ for line in rawdb.split("\n"):
 #print("]")
 
 if generate:
-	code = ["", "", "", "", "", ""]
-	
-	systems = []
-	for sys in codes.system_codes:
-		systems.append(sys["id"])
-	
-	print(codes.system_codes)
-	print(f"which system code (default {codes.system_codes[13]}):")
-	system = input()
-	if system == '' or system not in systems:
-		system = codes.system_codes[13]["id"]
+    code = ["", "", "", "", "", ""]
+    
+    systems = []
+    for sys in codes.system_codes:
+        systems.append(sys["id"])
+    
+    print(codes.system_codes)
+    print(f"which system code (default {codes.system_codes[13]}):")
+    system = input()
+    if system == '' or system not in systems:
+        system = codes.system_codes[13]["id"]
 
-	print(f"which title code (default random):")
-	title = input()
-	if title == '':
-		title = random.choices(codes.title_characters, k=2)
+    randtitle = False
+    print(f"which title code (default random):")
+    title = input()
+    if title == '':
+        title = random.choices(codes.title_characters, k=2)
+        randtitle = True
 
-	regions = []
-	for reg in codes.region_codes:
-		regions.append(reg["id"])
+    regions = []
+    for reg in codes.region_codes:
+        regions.append(reg["id"])
 
-	print(codes.region_codes)
-	print(f"which region code (default {codes.region_codes[3]}):")
-	region = input()
-	if region == '' or region not in regions:
-		region = codes.system_codes[2]["id"]
-	
-	for i in range(generatecount):
-		code[0] = system
-		code[1:2] = title
-		code[3] = region
-		print("".join(code))
+    print(codes.region_codes)
+    print(f"which region code (default {codes.region_codes[3]}):")
+    region = input()
+    if region == '' or region not in regions:
+        region = codes.system_codes[2]["id"]
+
+    publishers = []
+    for pub in codes.publisher_codes:
+        publishers.append(pub["id"])
+
+    print(codes.publisher_codes)
+    print(f"which publisher code(default {codes.publisher_codes[1]}):")
+    publisher = input()
+    if publisher == '' or publisher not in publishers:
+        publisher = codes.publisher_codes[1]["id"]
+    
+    count = 0
+    if randtitle == False:
+        count = 1
+    else:
+        count = generatecount
+
+    for i in range(count):
+        code[0] = system
+        code[1:2] = title
+        code[3] = region
+        code[4:2] = publisher
+        print("".join(code))
 
 if lookup:
-	print(f"searching for '{lookuptitle}'")
-	title_ids = []
-	for title in codes.title_codes:
-		title_ids.append(title["id"])
+    print(f"searching for '{lookuptitle}'")
+    title_ids = []
+    for title in codes.title_codes:
+        title_ids.append(title["id"])
 
-	lookupcount = 0
+    lookupcount = 0
+    idsearch = False
 
-	for title in codes.title_codes:
-		if lookuptitle.lower() in title["name"].lower():
-			index = title["name"].lower().find(lookuptitle.lower())
-			name = title["name"][:index] + "\033[1;32m" + lookuptitle.upper() + "\033[0m" + title["name"][(index + len(lookuptitle)):]
-						
-			print(str(title).replace(title["name"], name))
-				
-			lookupcount += 1
+    for tid in title_ids:
+        if lookuptitle in tid:
+            idsearch = True
 
-	print(f"found {lookupcount} matches")
+    if idsearch == True:
+        for title in codes.title_codes:
+            if lookuptitle in title["id"]:
+                index = title["id"].lower().find(lookuptitle.lower())
+                name = title["id"][:index] + "\033[1;32m" + lookuptitle.upper() + "\033[0m" + title["id"][(index + len(lookuptitle)):]
+
+                print(str(title).replace(title["id"], name))
+
+        lookupcount += 1
+    if idsearch == False:
+        for title in codes.title_codes:
+            if lookuptitle.lower() in title["name"].lower():
+                index = title["name"].lower().find(lookuptitle.lower())
+                name = title["name"][:index] + "\033[1;32m" + lookuptitle.upper() + "\033[0m" + title["name"][(index + len(lookuptitle)):]
+                        
+                print(str(title).replace(title["name"], name))
+                
+                lookupcount += 1
+
+    print(f"found {lookupcount} matches")
